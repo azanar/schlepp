638383faa04abb4d3ea8e80c58ee38ff33798482
Ed Carrel <edward@carrel.org>
2014-09-15 04:07:12 -0700

    Adding in a Collections class to the FS sink to impedence match Hydrogen again.

---
+++ lib/schlepp/sinks/fs/table_object/collection.rb
@@ -0,0 +1,22 @@
+
+module Schlepp
+  module Sink
+    module Fs
+      class TableObject
+        class Collection
+          include Hydrogen::TableObject::Collection
+
+          def url
+            "file:///#{path}"
+          end
+
+          def urls
+            parts.map do |p|
+              TableObject.new(p).url
+            end
+          end
+        end
+      end
+    end
+  end
+end
