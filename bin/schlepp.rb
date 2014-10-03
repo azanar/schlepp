af497e556a5961e27962c1c609b1e0fe03ec7d6b
Ed Carrel <edward@carrel.org>
2014-10-03 15:26:11 -0700

    Passing in a TableObject instead of a model breaks things, so we stop doing that.

--- bin/schlepp.rb
+++ bin/schlepp.rb
@@ -2,6 +2,8 @@
 $LOAD_PATH.push(File.dirname(__FILE__) + '/../lib')
 
 require 'hydrogen'
+require 'schlepp'
+
 require 'schlepp/sinks/fs'
 
 config = {
@@ -10,20 +12,16 @@ config = {
   :columns => %w{foo bar baz}
 }
 
-AWS.config(
-  access_key_id: 'ACCESS_KEY_ID',
-  secret_access_key: 'SECRET_ACCESS_KEY',
-  stub_requests:     Schlepp.env.test?,
-)
-
 model = Hydrogen::Model.new(config)
-
-to = Hydrogen::TableObject.new(model)
 
 source = Schlepp::Source::CSV.new(File.new('data.csv','r'))
 
-l = Schlepp::Sink::Fs::Sequencer.new(to, :chunk_size => 40000)
+l = Schlepp::Sink::Fs::Sequencer.new(model, :chunk_size => 40000)
 
 res = Schlepp.schlepp(source, l)
 
 puts res.inspect
+
+res.each do |r|
+  puts r.url.scheme
+end
