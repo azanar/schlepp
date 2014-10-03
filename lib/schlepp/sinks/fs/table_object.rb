b11ac2d32ccb7b63ca6188a8ed306ccc4ce91e3d
Ed Carrel <edward@carrel.org>
2014-10-03 15:37:46 -0700

    Removing dead code.

--- lib/schlepp/sinks/fs/table_object.rb
+++ lib/schlepp/sinks/fs/table_object.rb
@@ -4,26 +4,10 @@ module Schlepp
       class TableObject
         def initialize(table_object, opts = {})
           @table_object = table_object
-          @written = 0
         end
 
-        def write(rows)
-          Array(rows).each do |row|
-            @written += row.length
-            file.write(row)
-          end
-        end
-
-        def written
-          @written
-        end
-
-        def finalize
-          @file.close
-        end
-
-        def file
-          @file ||= File.new("#{@table_object.url.path}", 'w')
+        def url
+          URI("file:///tmp/#{@table_object.path}")
         end
       end
     end
