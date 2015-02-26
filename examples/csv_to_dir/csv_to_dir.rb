e26f1af63a93faf60f81772537bff5c9434518c9
Ed Carrel <edward@carrel.org>
2015-02-26 14:55:46 -0800

    Moving CSV to DIR example from submodule into root project.

---
+++ examples/csv_to_dir/csv_to_dir.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+$LOAD_PATH.push(File.dirname(__FILE__) + '/../lib')
+
+require 'hydrogen'
+require 'schlepp'
+require 'schlepp/sink/filter/chunker'
+require 'schlepp/sink/filter/compressor'
+
+require 'uri'
+require 'pp'
+
+require 'schlepp-sink-fs'
+
+config = {
+  :table_name => 'foo',
+  :source => {:file => 'data.csv'},
+  :columns => %w{foo bar baz}
+}
+
+model = Hydrogen::Model.new(config)
+
+source = Schlepp::Source::CSV.new(File.new('data.csv','r'))
+
+sink = Schlepp::Sink::Fs.new(model, URI('file:///tmp/foo/'),
+                             :filters => [Schlepp::Sink::Filter::Chunker.new(:chunk_size => 4000),
+                                          Schlepp::Sink::Filter::Compressor.new])
+
+res = Schlepp.schlepp(source, sink)
+
+
+res.each do |r|
+  puts r.url.path
+end
